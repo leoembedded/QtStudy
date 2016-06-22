dcf923df6a3e0b0a0195495efc53d74a386df485
leoembedded <leoandlucky@gmail.com>
2016-06-22 19:47:39 +0800

    获取IPv4地址

--- QtStudy/mainwindow.cpp
+++ QtStudy/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDialog>
+#include <QDebug>
+#include <QtNetwork>>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -8,11 +10,30 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    // 删除标题栏
+    setWindowFlags(Qt::FramelessWindowHint);
+
     // 增加按钮并修改显示内容 通过TR转码
     ui->newScreenButton->setText(tr("新窗口"));
 
     // 绑定信号和槽
     connect(ui->newScreenButton, SIGNAL(clicked()), this, SLOT(showChildDialog()));
+
+    // 获取本机名字
+    QString localHostName = QHostInfo::localHostName();
+    qDebug() <<"localHostName: "<<localHostName;
+
+    // 获取本机IP
+    QHostInfo info = QHostInfo::fromName(localHostName);
+    qDebug() <<"IP Address: "<<info.addresses();
+
+    // 只输出IPv4地址
+    foreach(QHostAddress address,info.addresses())
+    {
+         if(address.protocol() == QAbstractSocket::IPv4Protocol)
+            qDebug() << address.toString();
+    }
+
 }
 
 MainWindow::~MainWindow()
@@ -25,6 +46,7 @@ void MainWindow::showChildDialog()
 //    QDialog *dlg = new QDialog(this);
 //    dlg->show();
 
-LoginDialog.show();
+    LoginDialog.show();
+
 }
 
