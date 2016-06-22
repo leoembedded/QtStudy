4b87820e70eaf38e73afaf14bc5963e206699966
leoembedded <leoandlucky@gmail.com>
2016-06-22 11:43:58 +0800

    增加按键及中文显示
    Signed-off-by: leoembedded <leoandlucky@gmail.com>

--- QtStudy/main.cpp
+++ QtStudy/main.cpp
@@ -1,13 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QLabel>
+#include <QTextCodec>       // 设置编码格式头文件
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // 设置中文编码
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
     MainWindow w;
-    QLabel *label = new QLabel("Hello world QT!");
-    label->show();
     w.show();
 
     return a.exec();
--- QtStudy/mainwindow.cpp
+++ QtStudy/mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // 增加按钮并修改显示内容 通过tr转码
+    ui->newScreenButton->setText(tr("新窗口"));
 }
 
 MainWindow::~MainWindow()
